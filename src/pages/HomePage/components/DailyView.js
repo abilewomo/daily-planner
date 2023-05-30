3a2f4d427764f974e303724a15fe2845fc94fa17
abilewomo <abilewomo@gmail.com>
2023-05-30 11:04:47 -0400

    first commit

---
+++ src/pages/HomePage/components/DailyView.js
@@ -0,0 +1,8 @@
+export default function DailyView(){
+    
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/pages/HomePage/components/MonthlyView.js
@@ -0,0 +1,8 @@
+export default function MonthlyView(){
+    
+    return(
+        <div>
+            
+        </div>
+    )
+}
